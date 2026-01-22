fe5e74d7d87d6c6385795b66e42da58f47b159e1
saiedomar763-tech <saiedomar763@gmail.com>
2026-01-22 22:15:39 +0200

    Add files via upload

---
+++ datatype_practice.dart
@@ -0,0 +1,28 @@
+/* 1. Task 01:
+
+Create a small Dart file named
+c.
+
+Inside it:
+1. Declare these variables with the correct datatype:
+○ Your age
+○ Your GPA (example: 3.2)
+○ Your name
+○ Whether you are a developer (true/false)
+
+2. Print a sentence using all variables, like:
+a. My name is omar, I am 35 years old,
+my GPA is 3.4 and developer =
+true.");
+
+3. Change values and run again.*/
+void main() {
+  int age = 19;
+  double GPA = 2.0578;
+  String name = "saied";
+  bool iam_developer = true;
+
+  print(
+    "My name is $name , I am $age years old,my GPA is $GPA and developer =$iam_developer",
+  );
+}
